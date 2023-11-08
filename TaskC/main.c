f81e9e6ada91bc8d8eea971a4e545462a26a7f0a
jsum01 <djadjawltjr@gmail.com>
2023-11-08 21:05:01 +0900

    Initial Commit

---
+++ TaskC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TaskC
+//
+//  Created by 엄지석 on 11/8/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
